9531b12b602cd9135571056de1b22e00632c94b0
slacerda458-star <slacerda458@gmail.com>
2026-04-14 01:57:57 -0300

    primeiro teste do app

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+produtos = []
+
+@app.route("/")
+def home():
+    return "API Pricefly rodando!"
+
+@app.route("/produtos", methods=["GET"])
+def listar_produtos():
+    return jsonify(produtos)
+
+@app.route("/produtos", methods=["POST"])
+def adicionar_produtos():
+    data = request.json
+    produtos.append(data)
+    return jsonify({"msg": "Produto adicionado com sucesso"}),
+if __name__ == "__main__":
+    app.run(debug=True)
